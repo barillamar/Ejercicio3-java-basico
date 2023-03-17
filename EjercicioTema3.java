614bf225c92b1d6bb8cef507177d734788ca67f6
barillamar <127573905+barillamar@users.noreply.github.com>
2023-03-16 20:38:56 -0500

    Add files via upload

---
+++ EjercicioTema3.java
@@ -0,0 +1,12 @@
+package com.ejercicios;
+
+public class EjercicioTema3 {
+    public static void main(String[] args) {
+        //Crear un bucle que concatene textos e imprima el resultado final por consola
+
+        String[] textos = {"Rojo", "Anaranjado", "Amarillo", "Verde", "Azul"};
+        for (int i = 0; i < textos.length; i++) {
+            System.out.println(textos[i]);
+        }
+    }
+}
